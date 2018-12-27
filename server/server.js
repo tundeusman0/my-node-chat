e2919b6cbad4a99ce40132f3236dff428257263c
tundeUsman <tundeusman0@gmail.com>
2018-12-27 14:38:25 +0100

    Add connect and disconnect even handler

--- server/server.js
+++ server/server.js
@@ -1,15 +1,28 @@
 const express = require('express')
+const socketIo = require('socket.io')
 
 const path = require('path')
+const http = require('http')
 
 const publicPath = path.join(__dirname,'/../public')
 const port = process.env.PORT || 3000
 
 let app = express();
+let server = http.createServer(app)
+let io = socketIo(server)
 
 app.use(express.static(publicPath))
-console.log(publicPath)
 
-app.listen(port,()=>{
+
+io.on('connection',(socket)=>{
+    console.log('New user connected');
+    socket.on('disconnect', () => {
+        console.log('User Disconnected')
+    })
+})
+
+
+
+server.listen(port,()=>{
     console.log(`app has started ${port}`)
 })
