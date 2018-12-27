0aa8fd7887ac23970769ca0ad1f853e364371434
tundeUsman <tundeusman0@gmail.com>
2018-12-27 15:49:07 +0100

    Greet new User and alert others

--- server/server.js
+++ server/server.js
@@ -16,6 +16,16 @@ app.use(express.static(publicPath))
 
 io.on('connection',(socket)=>{
     console.log('New user connected');
+    socket.emit('newMessage',{
+        from:'Admin',
+        text:'Welcome to the chat app',
+        createdAt: new Date().getTime()
+    })
+    socket.broadcast.emit('newMessage',{
+        from:'Admin',
+        text:'New user joined',
+        createdAt: new Date().getTime()
+    })
     socket.on('createMessage',(message)=>{
         console.log('create message',message)
         io.emit('newMessage', {
